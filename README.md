1bb7c6f0123e61bb244d907767e2b81772d8f578
rt68 <54224071+rt68@users.noreply.github.com>
2023-12-04 17:18:53 -0500

    additional functionalities added

--- README.md
+++ README.md
@@ -1,3 +1,74 @@
 # rivergame
 Mod 1 the game
 This game is inspired by my childhood experience playing Oregon trail.  
+# The River Game
+
+## Inspired by the Oregon Trail
+
+Welcome to "The River Game," a text-based adventure inspired by the classic Oregon Trail. Experience the thrill of navigating the unpredictable wilderness, making crucial decisions, and facing challenges that will test your survival skills. Will you conquer the river and emerge victorious, or succumb to the perils of the trail?
+
+## Getting Started
+
+To embark on your journey, press the "Start" button. Enter your name and let the adventure begin! "The River Game" awaits, drawing inspiration from the historic Oregon Trail, with a modern twist in a DOM-based environment.
+
+## Player
+
+You play as a brave pioneer, equipped with the essentials for the journey:
+
+- **Name:** Your chosen identity
+- **Age:** A random age between 1 and 100
+- **Health:** Your initial health, determined by fate
+- **Attack Points:** Your skill in hunting for food
+- **Accuracy:** Precision in hunting, influenced by chance
+- **Ammo:** Limited ammunition for hunting
+- **Money:** Currency for transactions
+- **Food:** Your vital sustenance
+
+## Creatures
+
+Encounter a variety of creatures along the trail, each with its own challenges:
+
+- **Deer:**
+  - Health: 10
+  - Food Value: 10
+
+- **Rabbit:**
+  - Health: 5
+  - Food Value: 5
+
+- **Bear:**
+  - Health: 20
+  - Food Value: 20
+
+## Actions
+
+### Hunting
+
+Click "Hunt" to test your hunting skills. Success depends on accuracy and available ammunition. Successful hunts increase your food supply.
+
+### Crossing the River
+
+Face the critical decision of crossing the river. Choose to ford or ferry:
+
+- **Ford:** Attempt to cross the river, risking success or failure based on its depth.
+- **Ferry:** Spend 5 units of money for a safer river crossing.
+
+### Reloading
+
+If ammunition runs low during hunting, click "Reload" to replenish your supply.
+
+### Store
+
+Explore the in-game store to spend your hard-earned money on essential items.
+
+## River Game Features
+
+- **Randomized Challenges:** Encounter dynamic challenges inspired by the Oregon Trail.
+- **Strategic Decision-Making:** Choose your actions wisely to ensure survival.
+- **Inspired Setting:** Experience the spirit of the Oregon Trail with a modern, DOM-based twist.
+
+## Game Over
+
+If your health reaches zero or challenges prove insurmountable, the game concludes. Click "Reload" to restart your journey and conquer "The River Game."
+
+Prepare for an adventure reminiscent of the historic Oregon Trail but with a unique, modern twist. Make wise choices, overcome challenges, and triumph in "The River Game"!
--- script.js
+++ script.js
@@ -11,26 +11,41 @@ class Player {
     this.money = Math.floor(Math.random() * 10) + 10;
     this.food = Math.floor(Math.random() * 10) + 10;
   }
-  crossriver(target) {
+  fordriver(target) {
     if (Math.random() > target.depth / 10) {
+      pElement.innerText = `The river was ${target.depth} meters deep.  You successfully crossed the river.  You win!!`;
+      reloadBtn.style.display = "block";
+      fordBtn.style.display = "none";
+      ferryBtn.style.display = "none";
+
+    } else {
+      pElement.innerText = `The river was ${target.depth} meters deep.  You failed to cross the river.  You lose!!  Do you want to restart the game? `
+      reloadBtn.style.display = "block";
+      fordBtn.style.display = "none";
+      ferryBtn.style.display = "none";
+    }
+  }
+  
+  ferryriver() {
+    this.money -= 5
+    if (Math.random() > .1) {
       pElement.innerText = `You successfully crossed the river.  You win!!`;
       reloadBtn.style.display = "block";
-      huntBtn.style.display = "none";
-      riverBtn.style.display = "none";
-      
+      fordBtn.style.display = "none";
+      ferryBtn.style.display = "none";
     } else {
       pElement.innerText = `You failed to cross the river.  You lose!!  Do you want to restart the game? `
       reloadBtn.style.display = "block";
-      huntBtn.style.display = "none";
-      riverBtn.style.display = "none";
+      fordBtn.style.display = "none";
+      ferryBtn.style.display = "none";
     }
   }
-
   hunt(target) {
     if (this.ammo > 0) {
       this.ammo--;
     } else {
-      console.log("Out of ammo!");
+      pElement.innerText = "Out of ammo!";
+      huntBtn.style.display = "none";
       return;
     }
     if (Math.random() < this.accuracy) {
@@ -38,13 +53,16 @@ class Player {
       if (target.health <= 0) {
         target.health = 0;
         this.food += target.foodvalue;
-        pElement.innerText = `You successfully hunted ${target.name} and gained ${target.foodvalue} in food! What do you want to do now?`;
+        pElement.innerText = `You successfully hunted ${target.name} and gained ${target.foodvalue} in food! You have ${this.ammo} ammo left.  What do you want to do now?`;
         riverBtn.style.display = "inline";
       } else {
-        pElement.innerText = `You hunted ${target.name} and dealt ${this.attackpoints} damage! ${target.name} has ${target.health} health left.  Do you want to keep hunting?`;
-        riverBtn.style.display = "none";
+        pElement.innerText = `You hunted ${target.name} and dealt ${this.attackpoints} damage! ${target.name} has ${target.health} health left.  You have ${this.ammo} ammo left. Do you want to keep hunting?`;
+        riverBtn.style.display = "inline";
       }
 
+    } else {
+      pElement.innerText = `You missed!  You have ${this.ammo} ammo left.  Do you want to keep hunting?`;
+      riverBtn.style.display = "inline";
     }
   }
 }
@@ -64,6 +82,20 @@ class Deer {
   //     return true;
   //   }
   // }
+}
+class Rabbit {
+  constructor() {
+    this.name = "Rabbit";
+    this.health = 5;
+    this.foodvalue = 5;
+  }
+}
+class Bear {
+  constructor() {
+    this.name = "Bear";
+    this.health = 20;
+    this.foodvalue = 20;
+  }
 }
 class River {
   constructor() {
@@ -85,37 +117,100 @@ const startBtn = document.querySelector("#start");
 const huntBtn = document.querySelector("#hunt");
 const riverBtn = document.querySelector("#crossriver");
 const reloadBtn = document.querySelector("#reload");
+const fordBtn = document.querySelector("#ford");
+const ferryBtn = document.querySelector("#ferry");
 const pElement = document.querySelector("#message");
 const pictureEl = document.querySelector("#picture");
 const userformEl = document.querySelector("#userform");
 const usernameEl = document.querySelector("#name");
+const storeEl = document.querySelector("#store");
+const tableEl = document.querySelector("#table");
+const healthEl = document.querySelector("#health")
+const foodEl = document.querySelector("#food")
+const ammoEl = document.querySelector("#ammo")
+const moneyEl = document.querySelector("#money")
+
 
 
 huntBtn.style.display = "none";
 riverBtn.style.display = "none";
 reloadBtn.style.display = "none";
 userformEl.style.display = "none";
+fordBtn.style.display = "none";
+ferryBtn.style.display = "none";
 let username = usernameEl.value;
 let player1 = new Player(username);
-let deer1 = new Deer();
-let river1 = new River();
+const deer1 = new Deer();
+const rabbit1 = new Rabbit();
+const bear1 = new Bear();
+const river1 = new River();
+const hunted = [deer1, rabbit1, bear1];
+
+console.log(player1);
+
+healthEl.textContent = player1.health;
+ammoEl.textContent = player1.ammo;
+moneyEl.textContent = player1.money;
+foodEl.textContent = player1.food;
+
 console.log(deer1);
 console.log(river1);
+
 startBtn.addEventListener("click", () => {
   startBtn.style.display = "none";
   userformEl.style.display = "block";
   pElement.innerText = "Welcome to your new adventure.";
   pictureEl.src = "wagon.png";
 });
-huntBtn.addEventListener("click", () => {
-  player1.hunt(deer1);
-  pictureEl.src = "deer1.png";
 
+huntBtn.addEventListener("click", () => {
+  const random = Math.floor(Math.random() * hunted.length);
+  // console.log(random);
+
+  player1.hunt(hunted[random]);
+  
+  // console.log(hunted);
+  if (hunted[random].name === "Deer") {
+    pictureEl.src = "deer1.png";
+  } else if (hunted[random].name === "Rabbit") {
+    pictureEl.src = "rabbit1.png";
+  } else if (hunted[random].name === "Bear") {
+    pictureEl.src = "bear1.png";
+  }
+  if (hunted[random].health <= 0) {
+    if (hunted[random].name === "Deer") {
+      hunted.push(new Deer());
+    } else if (hunted[random].name === "Rabbit") {
+      hunted.push(new Rabbit());
+    
+    } else if (hunted[random].name === "Bear") {
+      hunted.push(new Bear());
+    }
+    hunted.splice(random, 1) ;
+    // console.log(hunted);
+  }
 });
+
 riverBtn.addEventListener("click", () => {
-  player1.crossriver(river1);
+  pElement.innerText = `The river is ${river1.depth} meters deep.  How do you want to proceed?`;
+  pictureEl.src = "riverchoice.jpg";
+  huntBtn.style.display = "none";
+  riverBtn.style.display = "none";
+  fordBtn.style.display = "inline";
+  ferryBtn.style.display = "inline";
+});
+fordBtn.addEventListener("click", () => {
+  player1.fordriver(river1);
   pictureEl.src = "river1.png";
 })
+ferryBtn.addEventListener("click", () => {
+  player1.ferryriver();
+  pictureEl.src = "ferry1.png";
+})
+// riverBtn.addEventListener("click", () => {
+//   player1.crossriver(river1);
+//   pictureEl.src = "river1.png";
+// })
 
 userformEl.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -131,4 +226,12 @@ userformEl.addEventListener("submit", (e) => {
 
 reloadBtn.addEventListener("click", () => {
   location.reload();
+});
+
+storeEl.addEventListener("click", () => {
+  pictureEl.src = "store1.png";
+  pElement.innerText = "Welcome to the store! What would you like to buy?";
+  tableEl.style.display = "flex";
+  // document.querySelectorAll("button").forEach((button) => {
+  //   style.display = "none";
 });
